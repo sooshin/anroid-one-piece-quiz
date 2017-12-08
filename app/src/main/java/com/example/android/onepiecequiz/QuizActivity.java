e80286c97d4a9d22d83efdeba09f26c9c5ec4cb1
Soojeong <soojeongshin2@gmail.com>
2017-12-08 23:26:31 +0900

    Set up-button programmatically

--- app/src/main/java/com/example/android/onepiecequiz/QuizActivity.java
+++ app/src/main/java/com/example/android/onepiecequiz/QuizActivity.java
@@ -3,6 +3,7 @@ package com.example.android.onepiecequiz;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
@@ -201,5 +202,20 @@ public class QuizActivity extends AppCompatActivity{
                 }
             }
         });
+
+        // Navigate with the app icon in the action bar
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+    }
+
+    // Go back to the previous screen when up button is clicked.
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
